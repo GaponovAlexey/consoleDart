c1f00e97078fea3dc9d7b8578857b089d69c0ea0
kekelberi <alexei.gaponov@gmail.com>
2023-04-09 20:45:01 -0500

    save

--- bin/im.dart
+++ bin/im.dart
@@ -1,9 +1,24 @@
-
-
 void main() {
-  print(test());
+  User bob = User("ivan", 22, true, ["walk", "game"]);
+  // ..name = "bobs2"
+  // ..age = 11;
+
+  print(bob.name);
+  print(bob.age);
+  print(bob.isHappy == true ? "hap" : "not");
+  print(bob.hobbies);
 }
 
-String test() {
-  return "da";
+class User {
+  String? name;
+  int? age;
+  bool? isHappy;
+  List<String>? hobbies;
+
+  User(name, [age, isHappy, hobbies]) {
+    this.name = name;
+    this.age = age;
+    this.isHappy = isHappy;
+    this.hobbies = hobbies;
+  }
 }
